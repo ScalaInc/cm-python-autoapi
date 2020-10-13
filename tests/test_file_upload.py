b5c8a10cacd1b0582e87aeb49bd91e78d0fb13ad
Greg Givler <greg.givler@scala.com>
2020-10-13 13:45:46 -0400

    fixed test_endpoint_file_upload_single_file() and moved the file test_file_upload.py to the tests directory. Both tests are now passing, also added a cleanup routine to delete the file being uploaded in the fixed test

--- tests/test_file_upload.py
+++ tests/test_file_upload.py
@@ -8,6 +8,8 @@ from framework.fileupload_rest import File_upload
 from framework.constants import CONFIG_FILE_PATH, LOG_FILE_PATH
 from framework.authentication_rest import login, logout, get_auth_token
 from framework.http_rest import *
+from framework.media_rest import Media
+
 import inspect
 import time
 
@@ -49,6 +51,8 @@ def t_setup():
     assert session is not None
     media_id_list = []
     user_id_list = []
+    api_version_fileupload = config['api_info']['api_version_fileupload']
+    api_version_media = config['api_info']['api_version_media']
 
 
 def t_teardown(upload_media=False):
@@ -60,6 +64,7 @@ def t_teardown(upload_media=False):
     setup_t
     '''
     global session
+
     logging.info('Beginning test teardown')
     response = logout(session, config['login']['baseurl'])
     assert response
@@ -125,8 +130,7 @@ def test_endpoint_file_upload_file():
     logging.info('Response  from delete call is: is: status_code =  {}, response = {}'.format(resp.status_code,resp.text))
     assert resp.status_code == 204, 'Received incorrect response code during media delete call.'
 
-@nottest
-@with_setup(t_setup,t_teardown)
+@with_setup(t_setup, t_teardown)
 def test_endpoint_file_upload_single_file():
     '''
     Begin a media upload of a single file using POST /api/rest/fileupload/init
@@ -137,47 +141,99 @@ def test_endpoint_file_upload_single_file():
     Delete the media item created in this test with DELETE /api/rest/media/{id}
     :return:
     '''
+    global session, baseurl, config, media_id_list, media_object
 
-    global session, baseurl, config
     current_time_since_epoch = time.time()
     local_file_name = config['media_items']['mediafile_2']
     file_upload_path = config['path']['media']
+    api_version_fileupload = config['api_info']['api_version_fileupload']
+    api_version_media = config['api_info']['api_version_media']
+    media_object = Media(api_version_media)
 
-    # Build JSON parameters for file upload in
+    # Build JSON parameters for file upload
     file_upload_parameter_list = {'filename': local_file_name, 'filepath': file_upload_path, 'uploadType': 'media_item'}
+    file_up = File_upload(api_version_fileupload)
 
-    # Begin Media upload.   Start with Init call
-    resp = rest_request(session, call_type.post, baseurl=config['login']['baseurl'], apiurl='/api/rest/fileupload/init',
-                        query_params = None, payload_params= file_upload_parameter_list,  proxy=False)
-    logging.info('Response from init call is: status_code =  {}, response = {}'.format(resp.status_code,resp.text))
-    assert resp.status_code == 200, 'Received incorrect response code after Media file upload init call.'
-    media_id = resp.json()['mediaId']
+    # Initiate Upload of media item
+    assert file_up.initiate_upload(session=session, baseurl=baseurl, local_file_name=local_file_name,
+                                   file_upload_path=file_upload_path)
 
-    # Save off the json response to pluck the uuid out of it
-    json_init_response = resp.json()
-    logging.debug('UUID from Init call is: {}'.format(json_init_response['uuid']))
-    logging.debug('filename from Init call is: {}'.format(json_init_response['filename']))
+    uuid = file_up.get_response_key('uuid')
+    media_id = file_up.get_response_key('mediaId')
+    media_id_list.append(media_id)
 
-    # Prep the arguments for the put call
-    file = open(config['path']['media'] + local_file_name, 'rb')
-    file_upload_put_apiurl = '/api/rest/fileupload/part/'+json_init_response['uuid'] + '/0'
+    # Upload file part
+    assert file_up.upload_file_part(session=session, baseurl=baseurl, local_file_name=local_file_name,
+                                    local_file_path=file_upload_path, uuid=uuid)
 
+    # Commit Upload
+    assert file_up.upload_finished(session=session, baseurl=baseurl, uuid=uuid)
 
-    #Send the put request to upload the file
-    resp = rest_request(session, call_type.post, baseurl=config['login']['baseurl'], apiurl=file_upload_put_apiurl, file_object=file)
-    logging.info('Response from file post call is: status_code =  {}, response = {}'.format(resp.status_code, resp.text))
-    file.close()
-    assert resp.status_code == 204, 'Received incorrect response code after file put call on media upload'
+    # Wait for upload to finish
+    assert media_object.wait_for_media_upload(session=session,
+                                              baseurl=baseurl,
+                                              max_wait_seconds=20,
+                                              media_id=media_id)
 
-    # Get the Media back
-    retrieve_media_apiurl = '/api/rest/media/' + str(media_id)
-    resp = rest_request(session, type_of_call=call_type.get, baseurl=baseurl, apiurl=retrieve_media_apiurl)
-    logging.info('Response  from media GET call is: is: status_code =  {}, response = {}'.format(resp.status_code,resp.text))
-    assert resp.status_code == 200, 'Received incorrect response code after media get.  Expected 200, received {}'.format(resp.status_code)
-    assert resp.json()['id'] == media_id, 'Incorrect media id retrieved by GET.  Expected {}, received {}'.format(media_id, resp.json()['id'])
+    logging.debug('media_id_list is = {}'.format(media_id_list))
 
-    # Delete media item to clean up
-    delete_apiurl = '/api/rest/media/' + str(media_id)
-    resp = rest_request(session, type_of_call=call_type.delete, baseurl = baseurl, apiurl=delete_apiurl)
-    logging.info('Response  from delete call is: is: status_code =  {}, response = {}'.format(resp.status_code,resp.text))
-    assert resp.status_code == 204, 'Received incorrect response code during media delete call.'
+    for identifier in media_id_list:
+        logging.debug('deleting media object where the id = {}'.format(identifier))
+        media_object.delete_media_by_id(session=session, baseurl=baseurl, id=identifier)
+
+# @nottest
+# @with_setup(t_setup,t_teardown)
+# def test_endpoint_file_upload_single_file():
+#     '''
+#     Begin a media upload of a single file using POST /api/rest/fileupload/init
+#     Retain the UUID from the response.  Retain the media id from the response
+#     Put the file up on the server using POST /api/rest/fileupload/part/{uuid}/0
+#     No need to commit the file when using the POST api call
+#     Verify the media item 'landed' by pulling it back with GET /api/rest/media/{id}
+#     Delete the media item created in this test with DELETE /api/rest/media/{id}
+#     :return:
+#     '''
+#
+#     global session, baseurl, config
+#     current_time_since_epoch = time.time()
+#     local_file_name = config['media_items']['mediafile_2']
+#     file_upload_path = config['path']['media']
+#
+#     # Build JSON parameters for file upload in
+#     file_upload_parameter_list = {'filename': local_file_name, 'filepath': file_upload_path, 'uploadType': 'media_item'}
+#
+#     # Begin Media upload.   Start with Init call
+#     resp = rest_request(session, call_type.post, baseurl=config['login']['baseurl'], apiurl='/api/rest/fileupload/init',
+#                         query_params = None, payload_params= file_upload_parameter_list,  proxy=False)
+#     logging.info('Response from init call is: status_code =  {}, response = {}'.format(resp.status_code,resp.text))
+#     assert resp.status_code == 200, 'Received incorrect response code after Media file upload init call.'
+#     media_id = resp.json()['mediaId']
+#
+#     # Save off the json response to pluck the uuid out of it
+#     json_init_response = resp.json()
+#     logging.debug('UUID from Init call is: {}'.format(json_init_response['uuid']))
+#     logging.debug('filename from Init call is: {}'.format(json_init_response['filename']))
+#
+#     # Prep the arguments for the put call
+#     file = open(config['path']['media'] + local_file_name, 'rb')
+#     file_upload_put_apiurl = '/api/rest/fileupload/part/'+json_init_response['uuid'] + '/0'
+#
+#
+#     #Send the put request to upload the file
+#     resp = rest_request(session, call_type.post, baseurl=config['login']['baseurl'], apiurl=file_upload_put_apiurl, file_object=file)
+#     logging.info('Response from file post call is: status_code =  {}, response = {}'.format(resp.status_code, resp.text))
+#     file.close()
+#     assert resp.status_code == 204, 'Received incorrect response code after file put call on media upload'
+#
+#     # Get the Media back
+#     retrieve_media_apiurl = '/api/rest/media/' + str(media_id)
+#     resp = rest_request(session, type_of_call=call_type.get, baseurl=baseurl, apiurl=retrieve_media_apiurl)
+#     logging.info('Response  from media GET call is: is: status_code =  {}, response = {}'.format(resp.status_code,resp.text))
+#     assert resp.status_code == 200, 'Received incorrect response code after media get.  Expected 200, received {}'.format(resp.status_code)
+#     assert resp.json()['id'] == media_id, 'Incorrect media id retrieved by GET.  Expected {}, received {}'.format(media_id, resp.json()['id'])
+#
+#     # Delete media item to clean up
+#     delete_apiurl = '/api/rest/media/' + str(media_id)
+#     resp = rest_request(session, type_of_call=call_type.delete, baseurl = baseurl, apiurl=delete_apiurl)
+#     logging.info('Response  from delete call is: is: status_code =  {}, response = {}'.format(resp.status_code,resp.text))
+#     assert resp.status_code == 204, 'Received incorrect response code during media delete call.'
